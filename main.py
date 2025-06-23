437192785272b1ebdeb5c77fd17038077871b2db
dbibb99 <dbibb99@gmail.com>
2025-06-23 12:45:29 -0400

    added error message and --verbose option

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    if len(sys.argv) > 1:
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        model = "gemini-2.0-flash-001"
+        user_prompt = sys.argv[1]
+        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-client = genai.Client(api_key=api_key)
+        response = client.models.generate_content(model=model, contents=messages)
 
+        if '--verbose' in sys.argv:
+            print(response.text)
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        else:
+            print(response.text)
 
-model = "gemini-2.0-flash-001"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    else:
+        print("Error: Must provide prompt")
+        print("Usage: python3 main.py <prompt>")
+        sys.exit(1)
 
-response = client.models.generate_content(model=model, contents=contents)
-
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__=="__main__":
+    main()
